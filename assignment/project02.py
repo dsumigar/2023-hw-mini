58bedac0d4fc6cb3c4cef1c0da986b2347e87114
dsumigar <114698332+dsumigar@users.noreply.github.com>
2023-09-19 14:09:56 -0400

    Update project02.py

--- assignment/project02.py
+++ assignment/project02.py
@@ -9,10 +9,8 @@ led = machine.Pin("LED", machine.Pin.OUT)
 button1 = machine.Pin(15, machine.Pin.IN, machine.Pin.PULL_UP)
 button2 = machine.Pin(16, machine.Pin.IN, machine.Pin.PULL_UP)
 adc = machine.ADC(2)
-N = 1
+N = 3
 sample_interval_s = 0.5
-t1_global = []
-t2_global = []
 
 def random_time_interval(tmin: float, tmax: float) -> float:
     """Return a random time interval between min and max."""
@@ -25,7 +23,7 @@ def light_logger(N: int, sample_interval_s: float) -> None:
         time.sleep(sample_interval_s)
     print(values)
     
-def reaction_game(N: int, t1_global, t2_global):
+def reaction_game(N: int):
     t1 = []
     t2 = []
     for _ in range(N):
@@ -43,30 +41,31 @@ def reaction_game(N: int, t1_global, t2_global):
 
         led.low()
         
-    t1_global = t1
-    t2_global = t2
-    return t1_global, t2_global
+    return t1, t2
 
 _thread.start_new_thread(light_logger, (N, sample_interval_s))
-reaction_game(N, t1_global, t2_global)
+t1, t2 = reaction_game(N)
 
-print(t1_global)
-sum_average_t1 = sum(t1_global)
-length_t1 = len(t1_global)
-avg_t1 = sum_average_t1 / length_t1
-print(f"This is the average for P1: {avg_t1}\n")
-max_value_t1 = max(t1_global)
-print(f"This is the max for P1: {max_value_t1}\n")
-min_value_t1 = min(t1_global)
-print(f"This is the min for P1: {min_value_t1}\n")
+print("t1", t1)
+if t1:	
+    sum_average_t1 = sum(t1)
+    length_t1 = len(t1)
+    avg_t1 = sum_average_t1 / length_t1
+    print(f"This is the average for P1: {avg_t1}\n")
+    max_value_t1 = max(t1)
+    print(f"This is the max for P1: {max_value_t1}\n")
+    min_value_t1 = min(t1)
+    print(f"This is the min for P1: {min_value_t1}\n")
 
-print(t2_global)
-sum_average_t2 = sum(t2_global)
-length_t2 = len(t2_global)
-avg_t2 = sum_average_t2 / length_t2
-print(f"This is the average for P2: {avg_t2}\n")
-max_value_t2 = max(t2_global)
-print(f"This is the max for P2: {max_value_t2}\n")
-min_value_t2 = min(t2_global)
-print(f"This is the min for P2: {min_value_t2}\n")
+print("t2", t2)
+if t2:
+    sum_average_t2 = sum(t2)
+    length_t2 = len(t2)
+    avg_t2 = sum_average_t2 / length_t2
+    print(f"This is the average for P2: {avg_t2}\n")
+    max_value_t2 = max(t2)
+    print(f"This is the max for P2: {max_value_t2}\n")
+    min_value_t2 = min(t2)
+    print(f"This is the min for P2: {min_value_t2}\n")
+
 
